5ee9cedab01987164a7f62b1ab904c8fd7ab5f1a
MaheninaRan <120193477+MaheninaRan@users.noreply.github.com>
2022-12-22 16:18:07 +0300

    Delete Client directory

--- Client/Main.java
+++
@@ -1,9 +0,0 @@
-package affichage;
-import listener.*;
-import graphique.*;
-public class Main {
-    public static void main(String[] args) throws Exception{
-        Fenetre frame=new Fenetre();
-       
-    }
-}
